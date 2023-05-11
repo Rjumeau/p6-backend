13f288457ad42b93f2fd5fd36930db25efd2a7b8
Romain Jumeau <romain.jumeau@hotmail.fr>
2023-05-11 15:01:49 +0200

    Models ok

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
 
@@ -12,5 +13,13 @@ app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
   next();
 });
+
+mongoose.connect('mongodb+srv://Romain01:secrette@atlascluster.7icvre6.mongodb.net/?retryWrites=true&w=majority',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
 
 module.exports = app;
