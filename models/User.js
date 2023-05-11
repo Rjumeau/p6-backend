174ccfd29234dd5adc888b4f2aaafc7680c82f60
Romain Jumeau <romain.jumeau@hotmail.fr>
2023-05-11 15:58:53 +0200

    middleware for auth and multer done

--- models/User.js
+++ models/User.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
 
+const uniqueValidator = require('mongoose-unique-validator');
+
 const userSchema = mongoose.Schema({
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true }
@@ -7,4 +9,4 @@ const userSchema = mongoose.Schema({
 
 userSchema.plugin(uniqueValidator);
 
-module.exports = mongoose.model('Sauce', sauceSchema);
+module.exports = mongoose.model('User', userSchema);
