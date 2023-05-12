e24596e08467cbba0e28834fcd805eac1ac526d2
Romain Jumeau <romain.jumeau@hotmail.fr>
2023-05-12 11:39:10 +0200

    sauce router and controller initialize

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const userRoutes = require('./routes/user');
+const sauceRoutes = require('./routes/sauce');
 require('dotenv').config();
 
 const app = express();
