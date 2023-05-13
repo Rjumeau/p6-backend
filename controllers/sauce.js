af9c1f200aa029cf512c75f9183a66b0e3948b3f
Romain Jumeau <romain.jumeau@hotmail.fr>
2023-05-13 11:53:32 +0200

    add delete sauce action

--- controllers/sauce.js
+++ controllers/sauce.js
@@ -1,4 +1,5 @@
 const Sauce = require('../models/sauce')
+const fs = require('fs');
 
 exports.getAllSauces = (req, res, next) => {
   Sauce.find().then(
@@ -76,3 +77,22 @@ exports.modifySauce = (req, res, next) => {
       res.status(400).json({ error });
     });
 };
+
+exports.deleteSauce = (req, res, next) => {
+  Sauce.findOne({ _id: req.params.id })
+    .then(sauce => {
+      if (sauce.userId != req.auth.userId) {
+        res.status(401).json({ message: 'Not authorized' });
+      } else {
+        const filename = sauce.imageUrl.split('/images/')[1];
+        fs.unlink(`images/${filename}`, () => {
+          Sauce.deleteOne({ _id: req.params.id })
+            .then(() => { res.status(200).json({ message: 'Sauce supprimée !' }) })
+            .catch(error => res.status(401).json({ error }));
+        });
+      }
+    })
+    .catch(error => {
+      res.status(500).json({ error });
+    });
+}
--- routes/sauce.js
+++ routes/sauce.js
@@ -10,6 +10,7 @@ router.get('/', auth, sauceCtrl.getAllSauces);
 router.get('/:id', auth, sauceCtrl.getOneSauce);
 router.post('/', auth, multer, sauceCtrl.createSauce);
 router.put('/:id', auth, multer, sauceCtrl.modifySauce);
+router.delete('/:id', auth, multer, sauceCtrl.deleteSauce)
 
 
 
